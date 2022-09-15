5609155eb285ffacd8d864ddc2f1a5f94dbb9372
Lyudmila <l.xmail@yandex.ru>
2022-09-15 19:40:05 +0300

    task3 completed

--- task3.py
+++ task3.py
@@ -1,3 +1,14 @@
 # Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и 
 # минимальным значением дробной части элементов.
 
+import math
+n = int(input('Введите кол-во чисел, которые вы собираетесь ввести: '))
+print ('Введите вещественные числа: ')
+newlist = list()
+for i in range(n):
+    newlist.append(float(input()))
+print (newlist)
+secondlist = list()
+for i in newlist:
+    secondlist.append(i%1)
+print (max(secondlist)-min(secondlist))
