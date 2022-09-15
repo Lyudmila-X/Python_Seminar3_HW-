f114b5a93347b959fb65e1b718bc373b19c5012c
Lyudmila <l.xmail@yandex.ru>
2022-09-15 19:46:33 +0300

    task4 completed

--- task4.py
+++ task4.py
@@ -1,1 +1,11 @@
 # Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+n = int(input("Введите число: "))
+binnum = ''
+if n==0:
+    print (0)
+else:
+    while n > 0:
+        binnum = str(n % 2) + binnum
+        n = n // 2
+    print(binnum)
